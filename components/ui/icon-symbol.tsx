018bea2ed8a060e7b56c321fdb4e22ed9070c42b
Anahí Foresi <anahiforesi@gmail.com>
2026-02-10 19:30:25 -0300

    Update icons mapping

--- components/ui/icon-symbol.tsx
+++ components/ui/icon-symbol.tsx
@@ -14,13 +14,44 @@ type IconSymbolName = keyof typeof MAPPING;
 /**
  * Add your SF Symbols to Material Icons mappings here.
  * - see Material Icons in the [Icons Directory](https://icons.expo.fyi).
- * - see SF Symbols in the [SF Symbols](https://developer.apple.com/sf-symbols/) app.
+ * - see SF Symbols in the [SF Symbols](https://developer.apple.com/sf-symbols/) app | [Web browsing](https://github.com/andrewtavis/sf-symbols-online/tree/master).
  */
 const MAPPING = {
   "house.fill": "home",
-  "paperplane.fill": "send",
-  "chevron.left.forwardslash.chevron.right": "code",
+  "arrow.left": "arrow-back",
   "chevron.right": "chevron-right",
+  xmark: "close",
+  "line.3.horizontal": "menu",
+  ellipsis: "more-horiz",
+  magnifyingglass: "search",
+  "cart.fill": "shopping-cart",
+  cart: "shopping-cart",
+  "person.fill": "person",
+  person: "person-outline",
+  "bell.fill": "notifications",
+  bell: "notifications-none",
+  "heart.fill": "favorite",
+  heart: "favorite-border",
+  "trash.fill": "delete",
+  trash: "delete-outline",
+  plus: "add",
+  minus: "remove",
+  "square.and.arrow.up": "share",
+  "star.fill": "star",
+  envelope: "mail-outline",
+  lock: "lock-outline",
+  "eye.fill": "visibility",
+  "eye.slash.fill": "visibility-off",
+  tshirt: "checkroom",
+  desktopcomputer: "computer",
+  house: "weekend",
+  "dumbbell.fill": "fitness-center",
+  "camera.fill": "camera-alt",
+  "gearshape.fill": "settings",
+  "creditcard.fill": "credit-card",
+  "box.truck.fill": "local-shipping",
+  "questionmark.circle.fill": "help",
+  "rectangle.portrait.and.arrow.right": "logout",
 } as IconMapping;
 
 /**
